279a1fdddd726a8c9da7c9346d28e1a3a2a5c821
Sean Bittner <sbittner@Seans-MacBook-Pro.local>
2018-10-28 12:58:54 -0400

    scripts

---
+++ dsn/scripts/init_nfs.py
@@ -0,0 +1,19 @@
+from dsn.train_dsn import initialize_nf
+import sys, os
+
+os.chdir("../")
+
+D = int(sys.argv[1]);
+nlayers = int(sys.argv[2]);
+sigma_init = float(sys.argv[3]);
+
+latent_dynamics = None;
+TIF_flow_type = 'PlanarFlowLayer';
+scale_layer = True;
+
+flow_dict = {'latent_dynamics':latent_dynamics, \
+             'TIF_flow_type':TIF_flow_type, \
+             'repeats':nlayers, \
+             'scale_layer':scale_layer};
+
+initialize_nf(D, flow_dict, sigma_init);
--- dsn/scripts/test_mvn.py
+++ dsn/scripts/test_mvn.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from dsn.util.systems import system_from_str
-from train_dsn import train_dsn
+from dsn.train_dsn import train_dsn
 import seaborn as sns
 import pandas as pd
 import scipy.stats
 import sys
 
-D = int(sys.argv[1]);o
+D = int(sys.argv[1]);
 c_init_order = int(sys.argv[2]);
 dist_seed = int(sys.argv[3]);
 random_seed = int(sys.argv[4]);
@@ -21,13 +21,14 @@ nlayers = 4;
 flow_dict = {'latent_dynamics':None, \
              'TIF_flow_type':TIF_flow_type, \
              'repeats':nlayers, \
-             'scale_layer':False};
+             'scale_layer':True};
 
 n = 1000;
 k_max = 10;
+sigma_init = 10;
 lr_order = -3;
-min_iters = 1000;
-max_iters = 2000;
+min_iters = 50000;
+max_iters = 100000;
 check_rate = 100;
 dir_str = 'convergence_testing';
 
@@ -52,6 +53,6 @@ print(Sigma[0]);
 print(Sigma[1]);
 
 cost, phi, T_x = train_dsn(system, behavior, n, flow_dict, \
-                       k_max=k_max, c_init_order=c_init_order, lr_order=lr_order,\
+                       k_max=k_max, sigma_init=sigma_init, c_init_order=c_init_order, lr_order=lr_order,\
                        random_seed=random_seed, min_iters=min_iters, max_iters=max_iters, \
                        check_rate=check_rate, dir_str=dir_str);
