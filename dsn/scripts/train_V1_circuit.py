e7757ec785929d58129c73377a80e8b1ac7e8676
Sean Bittner <sbittner@Seans-MacBook-Pro.local>
2019-01-19 23:06:21 -0500

    DSN V1 training script

--- dsn/scripts/train_V1_circuit.py
+++ dsn/scripts/train_V1_circuit.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from dsn.util.systems import V1_circuit
+from dsn.util.systems import V1Circuit
 from dsn.train_dsn import train_dsn
 import pandas as pd
 import scipy.stats
@@ -14,13 +14,15 @@ c_init_order = int(sys.argv[2]);
 sigma_init = float(sys.argv[3]);
 random_seed = int(sys.argv[4]);
 
-# normalizing flow layer architecture
-flow_type = 'PlanarFlowLayer'
-# number of layers
-flow_dict = {'latent_dynamics':None, \
-             'TIF_flow_type':flow_type, \
-             'repeats':nlayers, \
-             'scale_layer':True}
+D = 8
+latent_dynamics = None;
+TIF_flow_type = 'PlanarFlow';
+elem_mult_flow = True;
+arch_dict = {'D':D, \
+             'latent_dynamics':latent_dynamics, \
+             'elem_mult_flow':elem_mult_flow, \
+             'TIF_flow_type':TIF_flow_type, \
+             'repeats':nlayers};
 
 # create an instance of the V1_circuit system class
 fixed_params = {'W_EE':1.0, \
@@ -57,15 +59,15 @@ T = 40
 dt = 0.25
 init_conds = np.expand_dims(np.array([1.0, 1.1, 1.2, 1.3]), 1)
 
-system = V1_circuit(fixed_params, behavior, model_opts, T, dt, init_conds)
+system = V1Circuit(fixed_params, behavior, model_opts, T, dt, init_conds)
 
-k_max = 25
+k_max = 10
 batch_size = 1000;
 c_init_order = -5
 lr_order = -3
 
 
-train_dsn(system, batch_size, flow_dict, \
+train_dsn(system, batch_size, arch_dict, \
           k_max=k_max, sigma_init=sigma_init, c_init_order=c_init_order, lr_order=lr_order,\
           random_seed=random_seed, min_iters=5000, max_iters=10000, \
           check_rate=100, dir_str='V1_circuit')
