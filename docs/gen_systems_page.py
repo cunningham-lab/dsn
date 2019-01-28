f7d22c1d29bdf65bc01e6eb198e499ad9ce5201c
Sean Bittner <sbittner@dyn-160-39-152-214.dyn.columbia.edu>
2019-01-28 01:22:42 -0500

    automated documentation scripts

--- docs/gen_systems_page.py
+++ docs/gen_systems_page.py
@@ -13,9 +13,24 @@ permalink: /systems/
   <a href="#">DSN Systems Library</a>
 </div>
 
-Many commonly used models in theoretical neuroscience are already implemented as built-in system classes in the DSN library.
+Some neural circuit models from theoretical neuroscience are already implemented as built-in system classes in the DSN library.
 
 """
+
+footer_text = """
+
+# References #
+
+Dipoppa, Mario, et al. *[Vision and locomotion shape the interactions between neuron types in mouse visual cortex](https://www.sciencedirect.com/science/article/pii/S0896627318302435){:target="_blank"}*. Neuron 98.3 (2018): 602-615. <a name="Dipoppa2018Vision"></a>
+
+Pfeffer, Carsten K., et al. [Inhibition of inhibition in visual cortex: the logic of connections between molecularly distinct interneurons](https://www.nature.com/articles/nn.3446){:target="_blank"}*." Nature neuroscience 16.8 (2013): 1068. <a name="Pfeffer2013Inhibition"></a>
+
+Mastrogiuseppe, Francesca, and Srdjan Ostojic. *[Linking connectivity, dynamics, and computations in low-rank recurrent neural networks](https://www.sciencedirect.com/science/article/pii/S0896627318305439){:target="_blank"}*. Neuron 99.3 (2018): 609-623. <a name="Mastrogiuseppe2018Linking"></a>
+
+
+"""
+
+
 
 destfile = open(destfn, "w");
 destfile.write(header_text);
@@ -26,7 +41,7 @@ i = 1;
 for line in sourcefile:
 	if (line[:3] == "## "):
 		# write a big line to separate classes
-		destfile.write('\n*****\n');
+		### destfile.write('\n*****\n');
 		# fine underscores in class names
 		proc_line = line.replace('_', '\_');
 		# add ref tag
@@ -37,4 +52,5 @@ for line in sourcefile:
 	destfile.write(line);
 
 
+destfile.write(footer_text);
 
---
+++ docs/update_syspage.sh
@@ -0,0 +1,2 @@
+pydocmd simple dsn.util.systems++ > system_docs.md
+python3 gen_systems_page.py
