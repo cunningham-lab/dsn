8012fa8260df13493b8b6d1e7b0b592294807e5d
Sean Bittner <sbittner@Seans-MacBook-Pro.local>
2019-02-17 19:04:45 -0500

    tests for langevin dynamics bounds -- spont chaos and rank1 input

--- tests/test_langevin.py
+++ tests/test_langevin.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from dsn.util.tf_langevin import langevin_dyn
+from dsn.util.tf_langevin import langevin_dyn, langevin_dyn_spont_chaos, langevin_dyn_rank1_chaos
 from tf_util.stat_util import approx_equal
 
 EPS = 1e-16
@@ -56,6 +56,68 @@ def test_langevin_dyn():
 
     return None
 
+def test_langevin_dyn_spont_chaos():
+    n = 200
+    x0 = tf.placeholder(dtype=tf.float64, shape=(n, 3))
+
+    def f(x):
+        f1 = x[:, 1]+2
+        f2 = 0.0*x[:, 0]+1.1
+        f3 = x[:, 1]-1
+        return tf.stack([f1, f2, f3], axis=1)
+
+    eps = 0.8
+    num_its = 30
+
+    x_ss, x = langevin_dyn_spont_chaos(f, x0, eps, num_its, db=True)
+
+    _x0 = np.random.normal(0.0, 10.0, (n,3))
+
+    with tf.Session() as sess:
+        _x = sess.run(x, {x0:_x0})
+        
+    x_ss_true = np.array([3.1, 1.1, 0.1])
+
+    for i in range(n):
+        assert(approx_equal(_x[i,:,-1], x_ss_true, EPS))
+        assert(_x[i,1,1] >= 0.0)
+        assert(_x[i,2,1] >= 0.0)
+        assert(_x[i,1,1] >= _x[i,2,1])
+    return None
+
+
+
+def test_langevin_rank1_chaos():
+    n = 200
+    x0 = tf.placeholder(dtype=tf.float64, shape=(n, 4))
+
+    def f(x):
+        f1 = x[:, 2]+2
+        f2 = x[:, 2] - 5
+        f3 = 0.0*x[:, 0]+1.1
+        f4 = x[:, 2]-1
+        return tf.stack([f1, f2, f3, f4], axis=1)
+
+    eps = 0.8
+    num_its = 30
+
+    x_ss, x = langevin_dyn_rank1_chaos(f, x0, eps, num_its, db=True)
+
+    _x0 = np.random.normal(0.0, 10.0, (n,4))
+
+    with tf.Session() as sess:
+        _x = sess.run(x, {x0:_x0})
+        
+    x_ss_true = np.array([3.1, -3.9, 1.1, 0.1])
+
+    for i in range(n):
+        assert(approx_equal(_x[i,:,-1], x_ss_true, EPS))
+        assert(_x[i,2,1] >= 0.0)
+        assert(_x[i,3,1] >= 0.0)
+        assert(_x[i,2,1] >= _x[i,3,1])
+
 
 if __name__ == "__main__":
     test_langevin_dyn()
+    test_langevin_dyn_spont_chaos()
+    test_langevin_dyn_rank1_chaos()
