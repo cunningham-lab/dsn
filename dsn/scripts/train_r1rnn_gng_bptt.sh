46c4690e6fa0a9b44f337d34aa9d5390156831ee
Sean Bittner <sbittner@Seans-MacBook-Pro.local>
2019-02-23 22:52:14 +0100

    remove old rank-1 rnn training scripts from Sept

--- dsn/scripts/train_r1rnn_gng_bptt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --account=stats
-#SBATCH --job-name=gng_bptt
-#SBATCH -c 1
-#SBATCH --time=00:10:00
-#SBATCH --mem-per-cpu=1gb
-
-source activate dsn
-python3 train_r1rnn_gng_bptt.py $1
