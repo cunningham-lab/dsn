42cf0cac7f65fea8d9d24b8ab24a4d8d68b1dba2
Sean Bittner <seanrbittner@gmail.com>
2019-06-22 22:55:57 -0400

    script touch ups

--- dsn/scripts/train_STG_circuit.sh
+++ dsn/scripts/train_STG_circuit.sh
@@ -12,6 +12,5 @@
  
 module load singularity
 
-singularity exec --nv /moto/opt/singularity/tensorflow-1.13-gpu-py3-moto.simg python train_STG_circuit.py med $1 $2 $3
-$4 $5 $6
+singularity exec --nv /moto/opt/singularity/tensorflow-1.13-gpu-py3-moto.simg python train_STG_circuit.py med $1 $2 $3 $4 $5 $6
 
