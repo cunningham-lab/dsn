03bfa6f7edc697cc4fd3abca6cf58db195a4d974
Sean Bittner <seanrbittner@gmail.com>
2019-06-23 11:57:25 -0400

    shifted STG init

--- dsn/scripts/train_all_STG_circuit.sh
+++ dsn/scripts/train_all_STG_circuit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-for c_init in 2
+for K in 10 20
 do
   for rs in {1..2}
   do
-    for sigma_init in 3.0 5.0
+    for sigma_init in 2.0 3.0
     do 
-      for sigma0 in 0.1 0.5 1.0
+      for sigma0 in 0.1
       do
-        sbatch train_STG_circuit.sh 10 $c_init 4 $sigma_init $rs $sigma0
+        sbatch train_STG_circuit.sh 10 2 $K $sigma_init $rs $sigma0
       done
     done
   done
