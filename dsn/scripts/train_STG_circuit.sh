850f695b5fd30e0faaae733072345d2d01e63672
Sean Bittner <seanrbittner@gmail.com>
2019-06-22 22:45:38 -0400

    STG bash

--- dsn/scripts/train_STG_circuit.sh
+++ dsn/scripts/train_STG_circuit.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH --account=stats
-#SBATCH --job-name=STG_DSN
-#SBATCH -c 1
-#SBATCH --gres=gpu
-#SBATCH --time=11:59:00
+# Tensorflow with GPU support example submit script for Slurm.
+#
+# Replace <ACCOUNT> with your account name before submitting.
+#
+#SBATCH -A stats               # Set Account name
+#SBATCH --job-name=tensorflow  # The job name
+#SBATCH -c 1                   # Number of cores
+#SBATCH -t 0-11:30              # Runtime in D-HH:MM
+#SBATCH --gres=gpu:1           # Request a gpu module
 #SBATCH --mem-per-cpu=100gb
+ 
+module load singularity
 
-module load cuda90/toolkit
-module load cuda90/blas
-module load cudnn/7.0.5
-
-source activate dsn_gpu
-python3 train_STG_circuit.py $1 $2 $3 $4 $5 $6
+singularity exec --nv /moto/opt/singularity/tensorflow-1.13-gpu-py3-moto.simg python train_STG_circuit.py med $1 $2 $3 $4 $5
 
