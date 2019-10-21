cd3a640657b4c1014b0256614c7160ffa48f9b03
Sean Bittner <seanrbittner@gmail>
2019-10-20 20:42:53 -0400

    uncommented data/mu tests

--- tests/tst_V1Circuit.py
+++ tests/tst_V1Circuit.py
@@ -184,7 +184,6 @@ def test_V1Circuit():
 
     # Check that mu matches data
     # Local test. Cant put this data on github.
-    """
     s_list = [5, [5], [10], np.array([60])]
     fac_list = [1.0, 0.5, 100.0]
     num_s = len(s_list)
@@ -203,7 +202,7 @@ def test_V1Circuit():
             system = get_system_from_template(sysname, param_dict)
             assert(len(system.T_x_labels) == system.num_suff_stats)
             assert(system.mu.shape[0] == system.num_suff_stats)
-            approx_equal(mu_true, system.mu, EPS)"""
+            approx_equal(mu_true, system.mu, EPS)
 
 
 
