802ba8a9bc2698e8fa761d48216fae7bf511ea6e
Sean Robert Bittner <srb2201@columbia.edu>
2019-10-22 01:08:33 -0400

    haba LRRNN scripts

---
+++ dsn/scripts/train_LRRNN.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=stats
+#SBATCH --job-name=LRRNN
+#SBATCH -c 1
+#SBATCH --gres=gpu
+#SBATCH --time=1:59:00
+#SBATCH --mem-per-cpu=100gb
+
+module load cuda90/toolkit
+module load cuda90/blas
+module load cudnn/7.0.5
+
+source activate dsn_gpu
+
+python3 train_LRRNN.py $1 $2 $3 $4 $5
