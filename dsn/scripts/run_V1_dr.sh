3036c3ecc681fa4dd1321daf3d413db165241448
Sean Bittner <seanrbittner@gmail.com>
2020-04-30 11:47:26 -0400

    script updates

--- dsn/scripts/run_V1_dr.sh
+++ dsn/scripts/run_V1_dr.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=V1_dr
 #SBATCH -c 1
 #SBATCH --time=1:59:00
-#SBATCH --mem-per-cpu=2gb
+#SBATCH --mem-per-cpu=10gb
 
 source activate epi
 python3 V1_dr.py --alpha E --inc_val 0. --inc_std 0.01 --num_stages $1 --num_units $2 --logc0 0 --random_seed $3
